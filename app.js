c81880ee3a20f364d5f669bf72130fc1ed70a050
bhatnagaranshika02 <bhatnagaranshika02@gmail.com>
2020-09-15 10:25:52 +0530

    new

---
+++ app.js
@@ -0,0 +1,6 @@
+var mangoose = require('mongoose');
+var assert = require('assert');
+
+var Tutorials = require('./schema.js');
+
+var url = mongodb://localhost:27017/MyDb;
--- schema.js
+++ schema.js
@@ -1,1 +1,17 @@
-var mongoose = require('mongoose')
+var mongoose = require('mongoose');
+var assert = require('assert');
+var Schema = mongoose.Schema;
+var tutorialSchema = new Schema({
+   name:{
+      type:String,
+      require:true,
+      unique:true
+},
+    description:{
+           type: String,
+           required:true
+}
+}
+});
+var Tutorials = mongoose.model("tutorilas",tutorialSchema);
+module.exports = Tutorials;
